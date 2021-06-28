473064d3e5e5b426fe4e4abec3fab56670616d76
Mayank Goel <goel.mayank235@gmail.com>
2021-06-28 09:46:44 +0530

    Trying to add color and events

---
+++ randomcolor.js
@@ -0,0 +1,7 @@
+const colour = document.querySelector('button');
+function changecoor(){document.body.style.backgroungColor='blue';}
+colour.addEventListener('click', function (){
+    document.body.style.backgroundColor = "orange";
+    console.log("ey");
+});
+console.log("hiii");
