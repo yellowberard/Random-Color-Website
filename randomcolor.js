59d0f0c27d815be5e1168e5a6673717de07dac82
Mayank Goel <goel.mayank235@gmail.com>
2021-06-28 12:37:03 +0530

    Added Random color feature

--- randomcolor.js
+++ randomcolor.js
@@ -1,7 +1,16 @@
 const colour = document.querySelector('button');
-function changecoor(){document.body.style.backgroungColor='blue';}
-colour.addEventListener('click', function (){
-    document.body.style.backgroundColor = "orange";
-    console.log("ey");
+const  h1=document.querySelector('h1');
+const rndmcolor = () =>
+{
+   const r = Math.floor(Math.random()*255);
+   const g = Math.floor(Math.random()*255);
+   const b = Math.floor(Math.random()*255);
+   return `rgb(${r},${g},${b})`;
+}
+
+colour.addEventListener('click', function () {
+    const newcolor= rndmcolor();
+    document.body.style.backgroundColor = newcolor;
+    console.log("hey")
+    h1.innerText= `THE Color is : ${newcolor}`;
 });
-console.log("hiii");
