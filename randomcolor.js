d106a11c2225291fb1658ad10314e404ffb3c4f6
Mayank Goel <goel.mayank235@gmail.com>
2021-07-03 00:05:37 +0530

    centred buuton

--- randomcolor.js
+++ randomcolor.js
@@ -5,7 +5,7 @@ const rndmcolor = () =>
    const r = Math.floor(Math.random()*255);
    const g = Math.floor(Math.random()*255);
    const b = Math.floor(Math.random()*255);
-  
+//   let r=10, b =40 ,g=31;
    return `rgb(${r},${g},${b})`;
 }
 
@@ -14,5 +14,8 @@ colour.addEventListener('click', function () {
     document.body.style.backgroundColor = newcolor;
     console.log("hey")
     h1.innerText= `The Color is : ${newcolor}`;
-    
+    // if(`newcolor<rgb(50,50,50)`)
+    // {
+    //   h1.innerText.style.colour = white;  
+    // }
 });
