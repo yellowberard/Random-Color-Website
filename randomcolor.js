8fc36d9d1db6f82dbbd5888da6f77276b60e7e01
Mayank Goel <goel.mayank235@gmail.com>
2021-07-10 08:56:33 +0530

    Added Contrast Headings

--- randomcolor.js
+++ randomcolor.js
@@ -1,21 +1,21 @@
 const colour = document.querySelector('button');
-const  h1=document.querySelector('h1');
-const rndmcolor = () =>
-{
-   const r = Math.floor(Math.random()*255);
-   const g = Math.floor(Math.random()*255);
-   const b = Math.floor(Math.random()*255);
-//   let r=10, b =40 ,g=31;
-   return `rgb(${r},${g},${b})`;
+const h = document.querySelector('h1');
+const rndmcolor = () => {
+
+    let rgb = [1, 1, 1];
+    rgb[0] = Math.floor(Math.random() * 255);
+    rgb[1] = Math.floor(Math.random() * 255);
+    rgb[2] = Math.floor(Math.random() * 255);
+
+    return rgb;
 }
 
 colour.addEventListener('click', function () {
-    const newcolor= rndmcolor();
-    document.body.style.backgroundColor = newcolor;
-    console.log("hey")
-    h1.innerText= `The Color is : ${newcolor}`;
-    // if(`newcolor<rgb(50,50,50)`)
-    // {
-    //   h1.innerText.style.colour = white;  
-    // }
+    const newcolor = rndmcolor();
+    const newc = `rgb(${newcolor[0]},${newcolor[1]},${newcolor[2]})`;
+    const textc = Math.floor(((parseInt(newcolor[0]) * 299) + (parseInt(newcolor[1]) * 587) + (parseInt(newcolor[2]) * 114)) / 1000);
+    const textColour = (textc > 125) ? 'black' : 'white';
+    document.body.style.backgroundColor = newc;
+    h.style.color = textColour;
+    h.innerText = `THE COLOR IS: ${newcolor}`;
 });
