ba8b2c79b90da2e9b9b5ff625c8a75563949bd59
Mayank Goel <goel.mayank235@gmail.com>
2021-07-10 14:21:21 +0530

    Added two more features

--- randomcolor.js
+++ randomcolor.js
@@ -1,5 +1,8 @@
-const colour = document.querySelector('button');
+const colour = document.querySelector('#random');
 const h = document.querySelector('h1');
+const gradcolor = document.querySelector('#gradc');
+const buttoncolor = document.querySelector('.brndm')
+
 const rndmcolor = () => {
 
     let rgb = [1, 1, 1];
@@ -14,11 +17,41 @@ const checkContrast = (tcolor) => {
     const textColour = (textc > 96) ? 'black' : 'white';
     return textColour;
 }
+
 colour.addEventListener('click', function () {
     const tempcolor = rndmcolor();
     const newcolor = `rgb(${tempcolor[0]},${tempcolor[1]},${tempcolor[2]})`;
     const textcolor = checkContrast(tempcolor);
-    document.body.style.backgroundColor = newcolor;
+    document.body.style.background = newcolor;
     h.style.color = textcolor;
-    h.innerText = `THE COLOR IS: ${newcolor}`;
+    h.innerText = `THE BACKGROUND COLOR IS: ${newcolor}`;
+    buttoncolor.style.background = "white";
+
+});
+
+gradcolor.addEventListener('click', function () {
+    const tempcolor = rndmcolor();
+    const tempcolor2 = rndmcolor();
+    const newcolor = `rgb(${tempcolor[0]},${tempcolor[1]},${tempcolor[2]})`;
+    const newcolorr2 = `rgb(${tempcolor2[0]},${tempcolor2[1]},${tempcolor2[2]})`;
+
+    document.body.style.background = "linear-gradient(to right, " +
+        newcolor +
+        ", " +
+        newcolorr2 +
+        ")";
+
+    h.innerText = `THE GRADIENT COLOR IS: ${newcolor},${newcolorr2}`;
+    h.style.color = "black";
+    buttoncolor.style.background = "white";
+});
+
+buttoncolor.addEventListener('click', function () {
+    const tempcolor = rndmcolor();
+    const newcolor = `rgb(${tempcolor[0]},${tempcolor[1]},${tempcolor[2]})`;
+    const contrast = checkContrast(tempcolor);
+    document.body.style.background = contrast;
+    buttoncolor.style.background = newcolor;
+    h.style.color = newcolor;
+    h.innerText = `THE BUTTON BACKGROUND COLOR IS: ${newcolor}`;
 });
